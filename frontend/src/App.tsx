8d0bbb5e9206b0f4319dbccda52d701865706003
Martin Ng <martinng1992@gmail.com>
2021-05-01 00:06:25 -0400

    setup webpack for dev and prod

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 
+import './styles.css'
+
 export const App = () => {
-    return <h1>React TypeScript Webpack Template</h1>
+    return <h1>React TypeScript Webpack Template {process.env.NODE_ENV}  </h1>
 }

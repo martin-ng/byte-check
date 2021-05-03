f82c961fd3607d60656e26a984d878f1a5f29209
Martin Ng <martinng1992@gmail.com>
2021-05-03 16:01:41 -0400

    updated user model

--- bytecheck/settings.py
+++ bytecheck/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'graphene_django',
-    'user_controller'
+    'users',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
@@ -136,3 +136,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.ExtendUser'
--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserControllerConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_controller'
+    name = 'users'

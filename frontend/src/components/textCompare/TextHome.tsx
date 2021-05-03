4ef644e7e03514e6f740b0d6da63bc3c2db08573
Martin Ng <martinng1992@gmail.com>
2021-05-02 22:27:18 -0400

    created custom hook for texts

--- frontend/src/components/textCompare/TextHome.tsx
+++ frontend/src/components/textCompare/TextHome.tsx
@@ -1,10 +1,38 @@
-import React, { FC } from 'react'
+import React, { FC, useState, ChangeEvent } from 'react'
 import { TextInput } from "./TextHomeIndex"
 
 export const TextHome: FC = () => {
+
+    const useInput = (initialValue: any) => {
+        const [value, setValue] = useState<any>(initialValue)
+
+        const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+            setValue(event.target.value)
+        }
+        return [value, handleChange]
+    }
+
+    const [first, setFirst] = useInput('')
+    const [second, setSecond] = useInput('')
+    const [checking, setChecking] = useInput(false)
+    const [comparison, setComparison] = useInput([])
+    const [sameOne, setSameOne] = useInput([])
+    const [deleted, setDeleted] = useInput([])
+    const [sameTwo, setSameTwo] = useInput([])
+    const [added, setAdded] = useInput([])
+
+
+    const handleInputChange = (event: any) => {
+        event.preventDefault()
+    }
+
     return (
         <div>
-            <TextInput />
+            {/* <TextInput />
+             */}
+            {/* <input placeholder="first" type="text" value={first} onChange={setFirst}></input> */}
+            <input placeholder="second" type="text" value={second} onChange={setSecond}></input>
+
         </div>
     )
 }

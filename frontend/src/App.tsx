e973988812a5dc60624f8596f1a1e29dae96fb12
Martin Ng <martinng1992@gmail.com>
2021-05-02 20:07:04 -0400

    added react-router and working on navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react'
-import './styles.css'
+import './css/app.css'
 
 const { useState } = React
 
 export const App = () => {
-    const [count, setCount] = useState(0)
-    const name = "Martin"
 
-    return <h1>React TypeScript Webpack Template - {process.env.NODE_ENV} - {process.env.name}
-        <h2>{count}</h2>
-        <button onClick={() => setCount(count + 1)}></button>
-    </h1>
+    return (
+        <div>
+            <header>
+                <div className="logo">
+                    <h1>Byte Check</h1>
+                </div>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Login/Signup</a></li>
+                </ul>
+            </header>
+            <section>
+
+            </section>
+        </div>
+    )
 
 }

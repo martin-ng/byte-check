61a4695189315752d54c2ef1d448707eabc2af16
Martin Ng <martinng1992@gmail.com>
2021-05-03 14:06:11 -0400

    configured postgres

--- bytecheck/settings.py
+++ bytecheck/settings.py
@@ -24,7 +24,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -83,8 +82,12 @@ WSGI_APPLICATION = 'bytecheck.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASS"),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

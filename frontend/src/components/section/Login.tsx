b804d199d6fcee0d5f304e5aff4d4dd640c6612d
Martin Ng <martinng1992@gmail.com>
2021-05-10 19:41:22 -0400

    modified login page

--- frontend/src/components/section/Login.tsx
+++ frontend/src/components/section/Login.tsx
@@ -2,8 +2,8 @@ import React, { FC } from 'react'
 
 export const Login: FC = () => {
     return (
-        <div>
-            <h2>Login Page</h2>
-        </div>
+        <form>
+            <label htmlFor="username">Username</label>
+        </form>
     )
 }

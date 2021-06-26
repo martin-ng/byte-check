3740d46d42e4dfa375c51edc3bfb8ce3fa4c2dbb
Martin Ng <martinng1992@gmail.com>
2021-06-26 19:40:36 -0400

    added user schema

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from 'config';
 import log from './logger';
+import dbConnect from './db/connect';
 
 const port = config.get('port') as number;
 const host = config.get('host') as string;
@@ -12,4 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
     log.info(`Server is listening on http://${host}:${port}`);
+
+    dbConnect();
 });

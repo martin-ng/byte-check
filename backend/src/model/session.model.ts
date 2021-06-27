6b26320c45ec9c661859383071a04e1836b29fa1
Martin Ng <martinng1992@gmail.com>
2021-06-27 02:05:19 -0400

    added session mongoose schema

---
+++ backend/src/model/session.model.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { UserDoc } from './user.model';
+
+export interface SessionDoc extends mongoose.Document {
+    user: UserDoc['_id'];
+    valid: boolean;
+    userAgent: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const SessionSchema = new mongoose.Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        valid: { type: Boolean, default: true },
+        userAgent: { type: String }
+    },
+    { timestamps: true }
+);
+
+const Session = mongoose.model<SessionDoc>('Session', SessionSchema);

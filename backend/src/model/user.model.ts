5042d2c888da6c91b04623a5415a96e8426142c5
Martin Ng <martinng1992@gmail.com>
2021-06-26 20:53:28 -0400

    added user schema interface and methods

--- backend/src/model/user.model.ts
+++ backend/src/model/user.model.ts
@@ -2,6 +2,15 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import config from 'config';
 
+interface UserDoc extends mongoose.Document {
+    email: string;
+    name: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
 const UserSchema = new mongoose.Schema(
     {
         email: { type: String, required: true, unique: true },
@@ -11,4 +20,20 @@ const UserSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.pre('save', async function (next: mongoose.HookNextFunction) {
+    let user = this as UserDoc;
+
+    if (!user.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(config.get('saltWork'));
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword: string) {
+    const user = this as UserDoc;
+
+    return bcrypt.compare(candidatePassword, user.password).catch((error) => false);
+};
+
+const User = mongoose.model<UserDoc>('User', UserSchema);
+
+export default User;

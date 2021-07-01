97fa5e021ad20826a88aed8faddde5dd91cdd344
Martin Ng <martinng1992@gmail.com>
2021-06-30 23:19:19 -0400

    implementing text services

--- backend/src/controller/text.controller.ts
+++ backend/src/controller/text.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { get } from 'lodash';
 
-import { createText, findText } from '../service/text.service';
+import { createText, findText, findAndUpdateText } from '../service/text.service';
 
 export async function createTextController(req: Request, res: Response) {
     const userId = get(req, 'user._id');
@@ -14,7 +14,21 @@ export async function createTextController(req: Request, res: Response) {
 }
 
 export async function updateTextController(req: Request, res: Response) {
+    const userId = get(req, 'user._id');
     const textId = get(req, 'params.textId');
     const update = req.body;
+
     const text = await findText({ textId });
+
+    if (!text) {
+        return res.sendStatus(404);
+    }
+    console.log('text', text);
+    if (String(text.user) !== userId) {
+        return res.sendStatus(401);
+    }
+
+    const updatedText = await findAndUpdateText({ textId }, update, { new: true });
+    console.log('updatedText', updatedText);
+    return res.send(updatedText);
 }
--- backend/src/service/text.service.ts
+++ backend/src/service/text.service.ts
@@ -1,4 +1,4 @@
-import { DocumentDefinition, FilterQuery, QueryOptions } from 'mongoose';
+import { DocumentDefinition, FilterQuery, QueryOptions, UpdateQuery } from 'mongoose';
 
 import Text, { TextDoc } from '../model/texts.model';
 
@@ -6,8 +6,10 @@ export function createText(input: DocumentDefinition<TextDoc>) {
     return Text.create(input);
 }
 
-export function findText(
-    query: FilterQuery<TextDoc>, options: QueryOptions = { lean: true }
-) {
-    return Text.findOne(query, {}, options)
+export function findText(query: FilterQuery<TextDoc>, options: QueryOptions = { lean: true }) {
+    return Text.findOne(query, {}, options);
+}
+
+export function findAndUpdateText(query: FilterQuery<TextDoc>, update: UpdateQuery<TextDoc>, options: QueryOptions) {
+    return Text.findOneAndUpdate(query, update, options);
 }

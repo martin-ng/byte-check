b8619dc666edda7566776aa7f437ac47a79f5fa7
Martin Ng <martinng1992@gmail.com>
2021-07-11 19:48:28 -0400

    updated css for text input and view

--- frontend/src/components/section/Login.tsx
+++ frontend/src/components/section/Login.tsx
@@ -1,18 +1,33 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
 export const Login: FC = () => {
-    console.log("hello login")
+
+    const useInput = (initialValue: any) => {
+        const [value, setValue] = useState<any>(initialValue)
+
+        const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setValue(event.target.value)
+        }
+        return [value, handleChange]
+    }
+
+    const [email, setEmail] = useInput('')
+    const [password, setPassword] = useInput('')
+
+    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='form-inner'>
                 <h2>Login</h2>
                 <div className='form-group'>
                     <label htmlFor='email'>Email:</label>
-                    <input type='email' name='email' id='email' />
+                    <input type='email' name='email' id='email' onChange={setEmail} />
                 </div>
                 <div className='form-group'>
                     <label htmlFor='password'>Password:</label>
-                    <input type='password' name='password' id='password' />
+                    <input type='password' name='password' id='password' onChange={setPassword} />
                 </div>
             </div>
         </form>
